bb7dcfa4acc10bd303c1682c8981d97651f7ead6
CEFadiHamad <s12112857@stu.najah.edu>
2024-02-17 20:15:35 +0200

    (test-cases file)

--- src/test/java/cucumber/cucumber.java
+++ src/test/java/cucumber/cucumber.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "html:target/cucumberOutput.html" , monochrome = true ,snippets = CucumberOptions.SnippetType.CAMELCASE ,features = "use-cases",glue = "cucumber")
+@CucumberOptions(plugin = "html:outputTesting/cucumberOutput.html" , monochrome = true ,snippets = CucumberOptions.SnippetType.CAMELCASE ,features = "use-cases",glue = "cucumber")
 
 public class cucumber {
 
---
+++ src/test/java/cucumber/testcases.java
@@ -0,0 +1,430 @@
+package cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class testcases {
+
+    @Given("I am on the signup page")
+    public void i_am_on_the_signup_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I fill the signup form with valid details")
+    public void i_fill_the_signup_form_with_valid_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I submit the form")
+    public void i_submit_the_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should receive a confirmation email")
+    public void i_should_receive_a_confirmation_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be redirected to the login page")
+    public void i_should_be_redirected_to_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I fill the signup form with an email address that already exists")
+    public void i_fill_the_signup_form_with_an_email_address_that_already_exists() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see an error message indicating the email address is already in use")
+    public void i_should_see_an_error_message_indicating_the_email_address_is_already_in_use() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I fill the signup form with a weak password")
+    public void i_fill_the_signup_form_with_a_weak_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see an error message indicating the password requirements")
+    public void i_should_see_an_error_message_indicating_the_password_requirements() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I fill the signup form but leave some required fields blank")
+    public void i_fill_the_signup_form_but_leave_some_required_fields_blank() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see error messages indicating the missing fields")
+    public void i_should_see_error_messages_indicating_the_missing_fields() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I have filled in the signup form with valid details")
+    public void i_have_filled_in_the_signup_form_with_valid_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the confirmation email is delayed")
+    public void the_confirmation_email_is_delayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should have the option to request a resend of the confirmation email")
+    public void i_should_have_the_option_to_request_a_resend_of_the_confirmation_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Given("the user is in the sign-in page")
+    public void the_user_is_in_the_sign_in_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters their email address and password")
+    public void the_user_enters_their_email_address_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("clicks on the sign-in button")
+    public void clicks_on_the_sign_in_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should be redirected to their dashboard")
+    public void the_user_should_be_redirected_to_their_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the admin enters their admin email address and password")
+    public void the_admin_enters_their_admin_email_address_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the admin should be redirected to the admin dashboard")
+    public void the_admin_should_be_redirected_to_the_admin_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the service provider enters their email address and password")
+    public void the_service_provider_enters_their_email_address_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the service provider should be redirected to the service provider dashboard")
+    public void the_service_provider_should_be_redirected_to_the_service_provider_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters incorrect Email Address")
+    public void the_user_enters_incorrect_email_address() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should receive an error message indicating invalid Email Address")
+    public void the_user_should_receive_an_error_message_indicating_invalid_email_address() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters incorrect Password")
+    public void the_user_enters_incorrect_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should receive an error message indicating invalid Password")
+    public void the_user_should_receive_an_error_message_indicating_invalid_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on the {string} link")
+    public void the_user_clicks_on_the_link(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters their email address")
+    public void enters_their_email_address() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("submits the form")
+    public void submits_the_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should receive an email with instructions to reset their password")
+    public void the_user_should_receive_an_email_with_instructions_to_reset_their_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the user attempts to sign in with incorrect \\(Email Address or Password) multiple times")
+    public void the_user_attempts_to_sign_in_with_incorrect_email_address_or_password_multiple_times() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user account should be temporarily locked out")
+    public void the_user_account_should_be_temporarily_locked_out() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should receive a notification about the lockout time")
+    public void the_user_should_receive_a_notification_about_the_lockout_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I am in the event creation and management page")
+    public void i_am_in_the_event_creation_and_management_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click add choice")
+    public void i_click_add_choice() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Move to addition page")
+    public void move_to_addition_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click delete choice")
+    public void i_click_delete_choice() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Move to deletion page")
+    public void move_to_deletion_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click edit choice")
+    public void i_click_edit_choice() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Move to editing page")
+    public void move_to_editing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I am in addition page")
+    public void i_am_in_addition_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I fill the event information")
+    public void i_fill_the_event_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click add")
+    public void i_click_add() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("A successful message should appear")
+    public void a_successful_message_should_appear() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The new event should be added to event lists")
+    public void the_new_event_should_be_added_to_event_lists() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I am in deletion page")
+    public void i_am_in_deletion_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I select the wanted event to delete")
+    public void i_select_the_wanted_event_to_delete() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click delete")
+    public void i_click_delete() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The event should be deleted from event lists")
+    public void the_event_should_be_deleted_from_event_lists() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I am in editing page")
+    public void i_am_in_editing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I click edit the selected event")
+    public void i_click_edit_the_selected_event() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Show all event information in a distinct page to edit the wanted ones")
+    public void show_all_event_information_in_a_distinct_page_to_edit_the_wanted_ones() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I am in the distinct page that pop on the editing page")
+    public void i_am_in_the_distinct_page_that_pop_on_the_editing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I change the wanted event information")
+    public void i_change_the_wanted_event_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click edit")
+    public void i_click_edit() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The event in the event list should be updated")
+    public void the_event_in_the_event_list_should_be_updated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I fill the event information but leave some or all required fields blank")
+    public void i_fill_the_event_information_but_leave_some_or_all_required_fields_blank() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see error message")
+    public void i_should_see_error_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event addition page to complete the information")
+    public void the_program_returns_me_to_event_addition_page_to_complete_the_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("No changes done on the event")
+    public void no_changes_done_on_the_event() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Show me an information message")
+    public void show_me_an_information_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Dont do any thing")
+    public void dont_do_any_thing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("An error message should appear")
+    public void an_error_message_should_appear() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event addition page to repeat the date")
+    public void the_program_returns_me_to_event_addition_page_to_repeat_the_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event addition page to repeat the time")
+    public void the_program_returns_me_to_event_addition_page_to_repeat_the_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event addition page to repeat the attendee count")
+    public void the_program_returns_me_to_event_addition_page_to_repeat_the_attendee_count() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I change the event information")
+    public void i_change_the_event_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event editing page to repeat the date")
+    public void the_program_returns_me_to_event_editing_page_to_repeat_the_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event editing page to repeat the time")
+    public void the_program_returns_me_to_event_editing_page_to_repeat_the_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The program returns me to event editing page to repeat the attendee count")
+    public void the_program_returns_me_to_event_editing_page_to_repeat_the_attendee_count() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
