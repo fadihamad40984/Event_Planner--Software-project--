48027534a9b5b35be465c1515c68a1345deda7b8
fadihamad40984 <fadihamad40984@gmail.com>
2024-11-24 13:59:10 +0200

    add

---
+++ src/test/java/EventManagement/DeleteTester.java
@@ -0,0 +1,13 @@
+package EventManagement;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "html:outputTesting/deleteEvent.html" , features = {"Event_Management_Features/kdeleteEventService.feature"},
+        monochrome = true,snippets = CucumberOptions.SnippetType.CAMELCASE, glue = {"EventManagement"})
+
+public class DeleteTester {
+}
